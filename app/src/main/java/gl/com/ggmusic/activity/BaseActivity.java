1447293efdde4ecdfe1c63143073fbc0bcc596c2
guilin <973635949@qq.com>
2016-04-13 18:53:01 +0800

    底部布局
    Signed-off-by: guilin <973635949@qq.com>

--- app/src/main/java/gl/com/ggmusic/activity/BaseActivity.java
+++ app/src/main/java/gl/com/ggmusic/activity/BaseActivity.java
@@ -1,10 +1,11 @@
 package gl.com.ggmusic.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.ViewStub;
 
 import gl.com.ggmusic.R;
@@ -14,7 +15,7 @@ import gl.com.ggmusic.R;
  * 注意事项:
  * 1.构造方法中需调用setContentView()；
  */
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     /**
@@ -35,7 +36,7 @@ public abstract class BaseActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_base);
 
@@ -63,8 +64,8 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected void initToolBar(String title) {
         toolbar.setTitle(title);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setNavigationOnClickListener(this);
     }
 
     /**
@@ -84,6 +85,29 @@ public abstract class BaseActivity extends AppCompatActivity {
     abstract void setListener();
 
 
-    protected void goto
+    /**
+     * 去下一个Activitiy的快捷方法
+     *
+     * @param activityClass
+     * @param bundle
+     */
+    protected void startActivity(Class activityClass, Bundle bundle) {
+        Intent intent = new Intent(context, activityClass);
+        if (bundle != null)
+            intent.putExtras(bundle);
+        startActivity(intent);
+    }
 
+    protected void startActivity(Class activityClass) {
+        startActivity(activityClass, null);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
+    }
+
+    protected void setNavigationOnClickListener(View.OnClickListener listener){
+        toolbar.setNavigationOnClickListener(listener);
+    }
 }
--- app/src/main/java/gl/com/ggmusic/activity/MainActivity.java
+++ app/src/main/java/gl/com/ggmusic/activity/MainActivity.java
@@ -1,15 +1,24 @@
 package gl.com.ggmusic.activity;
 
 
-import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import gl.com.ggmusic.R;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-    private android.widget.TextView textView;
+
+    private ImageView menuImageView;
+    private ImageView discoverImageView;
+    private ImageView musicImageView;
+    private ImageView friendsImageView;
+    private ImageView searchImageView;
 
     public MainActivity() {
         setContentView(R.layout.activity_main);
@@ -17,31 +26,68 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     void init() {
-        toolbar.setVisibility(View.GONE);
+
+        this.searchImageView = (ImageView) findViewById(R.id.searchImageView);
+        this.friendsImageView = (ImageView) findViewById(R.id.friendsImageView);
+        this.musicImageView = (ImageView) findViewById(R.id.musicImageView);
+        this.discoverImageView = (ImageView) findViewById(R.id.discoverImageView);
+        this.menuImageView = (ImageView) findViewById(R.id.menuImageView);
 
 
-        textView = (TextView) findViewById(R.id.textView);
+        initBottomMusicView();
     }
+
+    /**
+     * 初始化底部音乐悬浮穿
+     */
+    private void initBottomMusicView() {
+        View musicView = LayoutInflater.from(context).inflate(R.layout.layout_bottom_music, null);
+        final TextView tv = (TextView) musicView.findViewById(R.id.musicNameTextView);
+        //TYPE_TOAST 是关键,这样就不需要悬浮窗权限了
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_TOAST);
+        //禁止获得焦点，不然下面的界面无法接收到触摸点击事件  无法接收onBack
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.format = PixelFormat.TRANSLUCENT;
+        params.gravity = Gravity.BOTTOM;
+
+        musicView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        WindowManager windowManager = (WindowManager) getApplication().getSystemService(getApplication().WINDOW_SERVICE);
+        windowManager.addView(musicView, params);
+    }
+
 
     @Override
     void initView() {
 
+        toolbar.setVisibility(View.GONE);
     }
 
     @Override
     void setListener() {
-        textView.setOnClickListener(this);
+        this.searchImageView.setOnClickListener(this);
+        this.friendsImageView.setOnClickListener(this);
+        this.musicImageView.setOnClickListener(this);
+        this.discoverImageView.setOnClickListener(this);
+        this.menuImageView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.textView:
-                startActivity(new Intent(MainActivity.this,SettingActivity.class));
-            break;
+            case R.id.menuImageView:
+                startActivity(SettingActivity.class);
+                break;
 
             default:
                 break;
         }
     }
+ 
+
 }
